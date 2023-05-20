15ef77682a4c989b1c1184d28c3a34e13c92e817
Oscar Martin Gonzalez-Chavez <o.martin9203@gmail.com>
2023-05-20 15:34:29 +0200

    feat: wip

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,9 +15,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   const host = process.env.HOST || 'http://localhost';
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: ${host}:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: ${host}:${port}/${globalPrefix}`);
 }
 
 bootstrap();
