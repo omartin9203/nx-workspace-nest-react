9441a8f626dc8ac030de766634e37d115d7c5f8c
Oscar Martin Gonzalez-Chavez <o.martin9203@gmail.com>
2023-05-22 03:11:12 +0200

    feat: wip

--- libs/internal-plugin/src/executors/fly-deploy/executor.ts
+++ libs/internal-plugin/src/executors/fly-deploy/executor.ts
@@ -5,14 +5,14 @@ export default async function runExecutor(options: FlyDeployExecutorSchema) {
   console.log('Executor ran for FlyDeploy -> ', options.distLocation);
   const cwd = options.distLocation;
   const results = execSync(`flyctl apps list`);
-  if (results.toString().includes(options.flyAppName)) {
-    execSync(`flyctl deploy`, { cwd });
-  } else {
-    // consult https://fly.io/docs/reference/regions/ to get best region for you
-    execSync(`flyctl launch --now --name=${options.flyAppName} --region=mad`, {
-      cwd,
-    });
-  }
+  // if (results.toString().includes(options.flyAppName)) {
+  //   execSync(`flyctl deploy`, { cwd });
+  // } else {
+  //   // consult https://fly.io/docs/reference/regions/ to get best region for you
+  //   execSync(`flyctl launch --now --name=${options.flyAppName} --region=mad`, {
+  //     cwd,
+  //   });
+  // }
   return {
     success: true,
   };
